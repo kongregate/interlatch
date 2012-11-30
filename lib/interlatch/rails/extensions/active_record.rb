9393b2d2b6b9e5a17711c087709fdc63bad836d4
Drew Chandler <drew@kongregate.com>
2012-11-30 14:11:16 -0800

    Expose methods for clearing caches from a model

--- lib/interlatch/rails/extensions/active_record.rb
+++ lib/interlatch/rails/extensions/active_record.rb
@@ -5,15 +5,22 @@ module Interlatch
         extend ActiveSupport::Concern
 
         included do
-          after_save :invalidate_caches
-          after_destroy :invalidate_caches
+          after_save :invalidate_interlatch_caches
+          after_destroy :invalidate_interlatch_caches
         end
 
-        def invalidate_caches
+        def invalidate_interlatch_caches
+          invalidate_interlatch_class_caches
+          invalidate_interlatch_instance_caches
+        end
+
+        def invalidate_interlatch_class_caches
           (::Rails.cache.fetch(Interlatch.dependency_key(self.class)) || []).each do |key|
             ::Rails.cache.delete(key)
           end
+        end
 
+        def invalidate_interlatch_instance_caches
           (::Rails.cache.fetch(Interlatch.dependency_key(self)) || []).each do |key|
             ::Rails.cache.delete(key)
           end
