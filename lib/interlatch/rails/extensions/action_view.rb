0a80a58327b62d6fbf3126e4ad2f213dabc14006
Matthew Wastrodowski <towski@gmail.com>
2013-02-12 16:11:59 -0800

    [#40715549] add a link to each view_cache element to show for admins

--- lib/interlatch/rails/extensions/action_view.rb
+++ lib/interlatch/rails/extensions/action_view.rb
@@ -12,6 +12,14 @@ module ActionView
          key = controller.caching_key(options[:tag], options[:scope])
          Interlatch.add_dependencies(key, args)
          cache(key, expires_in: options[:ttl], &block)
+         # doing this more simply fails for some reason
+         raise clear_link(key).inspect
+         @output_buffer = @output_buffer.nil? ? clear_link(key) : @output_buffer.to_s + clear_link(key)
+       end
+       
+       def clear_link(key)
+         link_to_function("clear key for #{key}", "active_user.eraseCacheKey('#{key}', event)", 
+          :class => :cache_link, :style => "style='color:#22B5BF; display:none'", :title => key )
        end
     end
   end
