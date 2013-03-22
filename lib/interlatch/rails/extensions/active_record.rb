6ebf8c83533321e2d8aa294a7f2674f7e189b393
Ben Munat <benm@kongregate.com>
2013-03-22 14:43:04 -0700

    Dup keys to deal with them being frozen.

--- lib/interlatch/rails/extensions/active_record.rb
+++ lib/interlatch/rails/extensions/active_record.rb
@@ -16,14 +16,14 @@ module Interlatch
 
         def invalidate_interlatch_instance_caches
           (::Rails.cache.read(Interlatch.dependency_key(self)) || []).each do |key|
-            ::Rails.cache.delete(key)
+            ::Rails.cache.delete(key.dup)
           end
         end
 
         module ClassMethods
           def invalidate_interlatch_class_caches
             (::Rails.cache.read(Interlatch.dependency_key(self)) || []).each do |key|
-              ::Rails.cache.delete(key)
+              ::Rails.cache.delete(key.dup)
             end
           end
         end
